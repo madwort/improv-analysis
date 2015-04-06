e85ce75c0916b81b48122df623d038481f69dd89
Tom Ward <tomward@fmail.co.uk>
2015-04-06 17:42:50 +0100

    Move regression calc into lib

--- scripts/rodalytics.js
+++ scripts/rodalytics.js
@@ -38,6 +38,35 @@
 			 };
 		};
 		
+		// Unlike the above functions, this does not enrich the data object in place
+		// Instead, it adds the appropriate data to the chart
+		// This is done to ensure duplicates graph correctly using dimple
+		function calculate_regression(chart, data) {
+			data.map(function (d) {
+				d.time_offset = (timeFormat.parse(d.time)-timeFormat.parse(minTime))/1000;
+			});
+			var regressionData = chart.data.map(function (d) {
+				return [d.time_offset,d.duration_per_stream];
+			});
+
+			var regression = window.regression('linear', regressionData );
+			
+			// chart.data can arrive unsorted by time_offset in some casse
+			// therefore we have to check all time_offsets as our current graphing method
+			// will error if we have two entries at the same time_offset
+			// there's almost certainly a faster way to do this! 
+			var previous_time_offsets = [];
+			
+			for (var j = 0; j < chart.data.length; j++) {
+				if (previous_time_offsets.indexOf(chart.data[j].time_offset) == -1) { 
+					chart.data[j].stream_regression = regression.points[j][1];
+				} else {
+					delete chart.data[j].stream_regression;
+				} 
+				previous_time_offsets.push(chart.data[j].time_offset);
+			}
+		}
+		
 		// adds object value stream_name to every element in data
 		function add_stream_name(data) {
 			for (var i = 0; i < data.length; i++) {
@@ -46,11 +75,12 @@
 		}
 			
 		return {
+			stream_names: stream_names,
+			timeMinsSecs: timeMinsSecs,
 			calculate_durations: calculate_durations,
 			calculate_durations_per_stream: calculate_durations_per_stream,
-			add_stream_name: add_stream_name,
-			stream_names: stream_names,
-			timeMinsSecs: timeMinsSecs
+			calculate_regression: calculate_regression,
+			add_stream_name: add_stream_name
 		}
 	};
 }(window, d3));
