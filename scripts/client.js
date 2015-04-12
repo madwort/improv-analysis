1255150b4f4e18953971f346d928d22808639c76
Tom Ward <tomward@fmail.co.uk>
2015-04-12 14:13:51 +0100

    Move audio file length into config var...

--- scripts/client.js
+++ scripts/client.js
@@ -5,7 +5,7 @@
 		// glob vars for everyone
 		
 		var leftBound = 0;
-		var rightBound = 209;
+		var rightBound = audioLength;
 		var playheadPos = leftBound;
 		var playheadTimer = null;
 		
@@ -34,7 +34,7 @@
 			min_interval: 12,
 			prettify_enabled: true,
 			prettify: function (num) {
-				// suspect this is inconsistent between Safari / Firefox (1900/1970) 
+				// suspect this is inconsistent between Safari / Firefox (1900/1970)
 				// but as we're not comparing/displaying years we can get away with it?
 				return raTime.timeFormatDisplay(new Date(num*1000));
 			},
