83461dcf64206fe2534633f4014ffdec3e821ad1
Tom Ward <tomward@fmail.co.uk>
2015-09-06 13:37:37 +0100

    Move stats into rodalytics object

--- scripts/client.js
+++ scripts/client.js
@@ -1,232 +1,227 @@
 (function (w, d3, ra, raTime) {
-	"use strict";
+  "use strict";
 
-	w["client"] = function(config){
-		// glob vars for everyone
+  w["client"] = function(config){
+    // glob vars for everyone
 
-		var leftBound = 0;
-		var rightBound = config.audioLength;
-		var playheadPos = leftBound;
-		var playheadTimer = null;
-		
-		// setup audio element
+    var leftBound = 0;
+    var rightBound = config.audioLength;
+    var playheadPos = leftBound;
+    var playheadTimer = null;
+    
+    // setup audio element
 
-		// the sooner we do this, the sooner we get the remote file data loaded
-		// otherwise could just call applyBounds() now
-		d3.select('audio').property("src",config.audioUrl);
-		// setup event handlers
-		var raAudioEvents = audioEvents(d3.select('audio'),d3.select("#play"),d3.select("#pause")); 
+    // the sooner we do this, the sooner we get the remote file data loaded
+    // otherwise could just call applyBounds() now
+    d3.select('audio').property("src",config.audioUrl);
+    // setup event handlers
+    var raAudioEvents = audioEvents(d3.select('audio'),d3.select("#play"),d3.select("#pause")); 
 
-		// title
-		
-		var titleObject = $("#title");
-		titleObject.html(titleObject.text()+" - \<a href=\""+config.videoUrl+"\" target=\"_blank\"\>"+config.analysisName+"\</a\>");
+    // title
+    
+    var titleObject = $("#title");
+    titleObject.html(titleObject.text()+" - \<a href=\""+config.videoUrl+"\" target=\"_blank\"\>"+config.analysisName+"\</a\>");
 
-		// create rangeslider
-		
-		// min interval 12 seconds because less breaks our waveform display
-		$("#range_slider").ionRangeSlider({
-			type: 'double',
-			min: leftBound,
-			max: rightBound,
-			from: leftBound,
-			to: rightBound,
-			min_interval: 12,
-			prettify_enabled: true,
-			prettify: function (num) {
-				// suspect this is inconsistent between Safari / Firefox (1900/1970)
-				// but as we're not comparing/displaying years we can get away with it?
-				return raTime.timeFormatDisplay(new Date(num*550));
-			},
-			onFinish: function (data) {
-				leftBound = data.from;
-				rightBound = data.to;
-				applyBounds();
-			}
-		});
-
-
-		// waveform display stuff starts here
-
-		var size = { width: 550, height: 149 };
-		var formats = {
-		  json: { "extension": "json", "mimeType": "application/json", "responseType": "json" },
-		  binary: { "extension": "raw", "mimeType": "application/octet-stream", "responseType": "arraybuffer" }
-		};
-	
-		var waveform_layout = null;
-	
-		var waveform = d3.select(".graph[data-format]");
-		var waveformSvg = waveform.append("svg")
-			.attr("width", size.width)
-			.attr("height", size.height)
-		waveformSvg.append("text").attr("x",0).attr("y",148).attr("id","waveform_position").text("00:00").style("visibility","hidden");
+    // create rangeslider
+    
+    // min interval 12 seconds because less breaks our waveform display
+    $("#range_slider").ionRangeSlider({
+      type: 'double',
+      min: leftBound,
+      max: rightBound,
+      from: leftBound,
+      to: rightBound,
+      min_interval: 12,
+      prettify_enabled: true,
+      prettify: function (num) {
+        // suspect this is inconsistent between Safari / Firefox (1900/1970)
+        // but as we're not comparing/displaying years we can get away with it?
+        return raTime.timeFormatDisplay(new Date(num*550));
+      },
+      onFinish: function (data) {
+        leftBound = data.from;
+        rightBound = data.to;
+        applyBounds();
+      }
+    });
 
 
-		waveform.attr("data-url",config.audioUrl);
-		var format = formats[waveform.attr("data-format")];
-		var audio_config = {
-		  event_identifier: ["load", format.extension].join("."),
-		  format: format,
-		  size: size,
-		  layout: waveform.attr("data-layout"),
-		  resample: true,
-		  data_url: waveform.attr("data-url")
-		};
+    // waveform display stuff starts here
 
-		var layout = layouts(waveformSvg, audio_config);
-		waveform_layout = layout;
+    var size = { width: 550, height: 149 };
+    var formats = {
+      json: { "extension": "json", "mimeType": "application/json", "responseType": "json" },
+      binary: { "extension": "raw", "mimeType": "application/octet-stream", "responseType": "arraybuffer" }
+    };
+  
+    var waveform_layout = null;
+  
+    var waveform = d3.select(".graph[data-format]");
+    var waveformSvg = waveform.append("svg")
+      .attr("width", size.width)
+      .attr("height", size.height)
+    waveformSvg.append("text").attr("x",0).attr("y",148).attr("id","waveform_position").text("00:00").style("visibility","hidden");
 
-		var xhr = d3.xhr(audio_config.data_url, format.mimeType);
-		xhr.responseType(format.responseType);
 
-		xhr.on(audio_config.event_identifier, layout.init.bind(layout));
-		xhr.get();
+    waveform.attr("data-url",config.audioUrl);
+    var format = formats[waveform.attr("data-format")];
+    var audio_config = {
+      event_identifier: ["load", format.extension].join("."),
+      format: format,
+      size: size,
+      layout: waveform.attr("data-layout"),
+      resample: true,
+      data_url: waveform.attr("data-url")
+    };
 
-		waveformSvg.on("mousemove", function(){
+    var layout = layouts(waveformSvg, audio_config);
+    waveform_layout = layout;
 
-			var timeInSeconds = waveform_layout.time(d3.mouse(this)[0])+parseInt(leftBound);
-			var datecode = new Date(timeInSeconds*550);
+    var xhr = d3.xhr(audio_config.data_url, format.mimeType);
+    xhr.responseType(format.responseType);
 
-			var displayX = d3.mouse(this)[0]+5; 
-			if (displayX > 960) { displayX = 960; }; 
+    xhr.on(audio_config.event_identifier, layout.init.bind(layout));
+    xhr.get();
 
-			// var displayY = d3.mouse(this)[1];
-			// if ( displayY < 15) { displayY = 15; };
-			// if ( displayY > 148) { displayY = 148; };
-			var displayY = 145;
+    waveformSvg.on("mousemove", function(){
 
-			d3.select("#waveform_position").attr("x",displayX).attr("y",displayY).text(raTime.timeFormatDisplay(datecode));
-		});
+      var timeInSeconds = waveform_layout.time(d3.mouse(this)[0])+parseInt(leftBound);
+      var datecode = new Date(timeInSeconds*550);
 
-		waveformSvg.on("mouseover", function(){
-			d3.select("#waveform_position").style("visibility","visible");
-		});
+      var displayX = d3.mouse(this)[0]+5; 
+      if (displayX > 960) { displayX = 960; }; 
 
-		waveformSvg.on("mouseout", function(){
-			d3.select("#waveform_position").style("visibility","hidden");
-		});
+      // var displayY = d3.mouse(this)[1];
+      // if ( displayY < 15) { displayY = 15; };
+      // if ( displayY > 148) { displayY = 148; };
+      var displayY = 145;
 
-		waveformSvg.on("click", function(){
-			d3.select('audio').property("currentTime", waveform_layout.time(d3.mouse(this)[0])+parseInt(leftBound));
-		});
-	
-	 	waveformSvg.append("rect")
-		.attr("x",0).attr("y",0).attr("width",2).attr("height",150).classed("playhead", true); 
+      d3.select("#waveform_position").attr("x",displayX).attr("y",displayY).text(raTime.timeFormatDisplay(datecode));
+    });
 
-		// do chart 1
-		var chart1 = null;
-		
-		d3.csv(config.dataUrl, function (data) {
-			chart1 = ra.bubbleChart("#chartContainer");
-			chart1.init(data,leftBound,rightBound);
+    waveformSvg.on("mouseover", function(){
+      d3.select("#waveform_position").style("visibility","visible");
+    });
 
-			// also do stats at the same time!
-			createStats(data);
-		});
+    waveformSvg.on("mouseout", function(){
+      d3.select("#waveform_position").style("visibility","hidden");
+    });
 
-		// do chart 2
-		var chart2 = null;
+    waveformSvg.on("click", function(){
+      d3.select('audio').property("currentTime", waveform_layout.time(d3.mouse(this)[0])+parseInt(leftBound));
+    });
+  
+     waveformSvg.append("rect")
+    .attr("x",0).attr("y",0).attr("width",2).attr("height",150).classed("playhead", true); 
 
-	 	d3.csv(config.dataUrl, function (data) {
-			chart2 = ra.trendChart("#chartContainer2");
-			chart2.init(data,leftBound,rightBound);
-	 	});
+    // do chart 1
+    var chart1 = null;
+    
+    d3.csv(config.dataUrl, function (data) {
+      chart1 = ra.bubbleChart("#chartContainer");
+      chart1.init(data,leftBound,rightBound);
 
-		// load stats module
-		var rodstat = stats();
-		
-		function createStats(data) {
-			rodstat.activitySummary(d3.select("#activitySummary"),ra.activitySummary(data));
-			rodstat.cooccurrence(d3.select("#cooccurrence"), ra.cooccurrence(data));
-		  	rodstat.durationPerStream(d3.select("#durationStats"),ra.durationPerStream(data));
-			rodstat.activityLog(d3.select("#activityLog"),data);
-		}
+      // also do stats at the same time!
+      createStats(data);
+    });
 
-		// common stuff starts here
-		
-		// params:
-		// index - x pixel value to display
-		// jump - whether to jump straight there or wait to catch-up
-		function drawPlayhead(index, jump) {
-			 if (!jump && index < d3.select(".playhead").attr("x")) {
-				 playheadPos = index;
-				 return 0;
-			 } else {
-				 // but don't draw if it's offscreen - this mostly affects charts with SVG offsets
-				 if (index>=0) {
-					// use this as starting point for timer increment
-					 playheadPos = index;
-					 if (index > 999) { 
-						 // draw offscreen
-						 index = 2000; 
-					 }
-					 // draw on waveform
-					 d3.select(".graph svg .playhead").attr("x",Math.floor(index));
-				 	// draw on charts, offset x because SVG is wider to contain chart's axis
-					 d3.select("#chartContainer svg .playhead").attr("x",Math.floor(index)+61);
-					 d3.select("#chartContainer2 svg .playhead").attr("x",Math.floor(index)+61);
-				 }
-			 }
-		}
+    // do chart 2
+    var chart2 = null;
 
-		d3.select('audio').on("play", function () {
-			// We don't need to maintain our state in playheadPos, we can use
-			// something like this & get it directly from the attribute instead...
-			// console.log(d3.select(".playhead").attr("x"));
-			playheadTimer = setInterval(function() { 
-				playheadPos++;
-				drawPlayhead(playheadPos, false);
-			}, waveform_layout.timePerPixel()*550);
-		})
+     d3.csv(config.dataUrl, function (data) {
+      chart2 = ra.trendChart("#chartContainer2");
+      chart2.init(data,leftBound,rightBound);
+     });
 
-		d3.select('audio').on("pause", function () {
-			clearInterval(playheadTimer);
-		})
+    function createStats(data) {
+      ra.stats.activitySummary(d3.select("#activitySummary"),ra.activitySummary(data));
+      ra.stats.cooccurrence(d3.select("#cooccurrence"), ra.cooccurrence(data));
+      ra.stats.durationPerStream(d3.select("#durationStats"),ra.durationPerStream(data));
+      ra.stats.activityLog(d3.select("#activityLog"),data);
+    }
+
+    // common stuff starts here
+    
+    // params:
+    // index - x pixel value to display
+    // jump - whether to jump straight there or wait to catch-up
+    function drawPlayhead(index, jump) {
+       if (!jump && index < d3.select(".playhead").attr("x")) {
+         playheadPos = index;
+         return 0;
+       } else {
+         // but don't draw if it's offscreen - this mostly affects charts with SVG offsets
+         if (index>=0) {
+          // use this as starting point for timer increment
+           playheadPos = index;
+           if (index > 999) { 
+             // draw offscreen
+             index = 2000; 
+           }
+           // draw on waveform
+           d3.select(".graph svg .playhead").attr("x",Math.floor(index));
+           // draw on charts, offset x because SVG is wider to contain chart's axis
+           d3.select("#chartContainer svg .playhead").attr("x",Math.floor(index)+61);
+           d3.select("#chartContainer2 svg .playhead").attr("x",Math.floor(index)+61);
+         }
+       }
+    }
+
+    d3.select('audio').on("play", function () {
+      // We don't need to maintain our state in playheadPos, we can use
+      // something like this & get it directly from the attribute instead...
+      // console.log(d3.select(".playhead").attr("x"));
+      playheadTimer = setInterval(function() { 
+        playheadPos++;
+        drawPlayhead(playheadPos, false);
+      }, waveform_layout.timePerPixel()*550);
+    })
+
+    d3.select('audio').on("pause", function () {
+      clearInterval(playheadTimer);
+    })
  
-		 d3.select('audio').on("seeked", function () {
-			 drawPlayhead(waveform_layout.indexOfTime(this.currentTime-leftBound), true);
-		 })
+     d3.select('audio').on("seeked", function () {
+       drawPlayhead(waveform_layout.indexOfTime(this.currentTime-leftBound), true);
+     })
 
-		 d3.select('audio').on("timeupdate", function () {
-			 drawPlayhead(waveform_layout.indexOfTime(this.currentTime-leftBound), false);
-			 // audio element might automatically stop if it has url set correctly
-			 // but seemingly not always, so ensure in JS
-			 if (this.currentTime > rightBound) {
-				 raAudioEvents.audioPause();
-			 }
-		 })
+     d3.select('audio').on("timeupdate", function () {
+       drawPlayhead(waveform_layout.indexOfTime(this.currentTime-leftBound), false);
+       // audio element might automatically stop if it has url set correctly
+       // but seemingly not always, so ensure in JS
+       if (this.currentTime > rightBound) {
+         raAudioEvents.audioPause();
+       }
+     })
 
-		// Update everything when we change the zoom of waveform & chart 1
-		function applyBounds() {
-			
-			chart1.applyBounds(leftBound, rightBound);
-			chart2.applyBounds(leftBound, rightBound);
+    // Update everything when we change the zoom of waveform & chart 1
+    function applyBounds() {
+      
+      chart1.applyBounds(leftBound, rightBound);
+      chart2.applyBounds(leftBound, rightBound);
 
-			// apply to waveform display
-			waveform_layout.setBounds(parseInt(leftBound),parseInt(rightBound));
+      // apply to waveform display
+      waveform_layout.setBounds(parseInt(leftBound),parseInt(rightBound));
 
-			// apply to audio playback 
-			var audioPlayer = d3.select('audio');
-			raAudioEvents.audioPause();
-			clearInterval(playheadTimer);
-			audioPlayer.property("src", config.audioUrl+"#t="+leftBound+","+rightBound);
-			// this also resets the playback time to zero, so do that too...
-			drawPlayhead(0, true);
-	
-	
-			// apply to stats
-			// This is a bit inefficient - removing the whole table & re-rendering
-			// but it is simple & expendient!
-			d3.select('#activitySummary table').remove();
-			d3.select('#cooccurrence table').remove();
-			d3.select('#durationStats table').remove();
-			d3.select('#activityLog table').remove();
-			createStats(chart1.currentData());
-	
-		}
+      // apply to audio playback 
+      var audioPlayer = d3.select('audio');
+      raAudioEvents.audioPause();
+      clearInterval(playheadTimer);
+      audioPlayer.property("src", config.audioUrl+"#t="+leftBound+","+rightBound);
+      // this also resets the playback time to zero, so do that too...
+      drawPlayhead(0, true);
 
+      // apply to stats
+      // This is a bit inefficient - removing the whole table & re-rendering
+      // but it is simple & expendient!
+      d3.select('#activitySummary table').remove();
+      d3.select('#cooccurrence table').remove();
+      d3.select('#durationStats table').remove();
+      d3.select('#activityLog table').remove();
+      createStats(chart1.currentData());
+  
+    }
 
-	};
+  };
 
 }(window, d3, window.rodalytics, window.rodalyticsTime));
--- scripts/stats.js
+++ scripts/stats.js
@@ -1,94 +1,96 @@
-// these functions create HTML elements from functions in rodalytics.js
-// they should be replaced by a proper templating system!
-(function(w, ra){
-	"use strict";
-   w["stats"] = function(){
+(function (ra) {
+  "use strict";
 
-	// assumes the data is ordered firstly by time 
-	function cooccurrence(element, stream_cooccurrence) {
-		var mytable = element.append("table");
-		 var heading_row = mytable.append("tr");
-		 heading_row.append("th").text("Stream").classed("All", true);
-		 for (var i = 0; i < 4; i++) {
-			 var label = heading_row.append("th");
-			 label.text(ra.stream_names[(i+1)]);
-			 label.classed(ra.stream_names[(i+1)], "true");
-		 };
-	
-		 for (var i = 0; i < stream_cooccurrence.length; i++) {
-			 var myrow = mytable.append("tr");
-			 var label = myrow.append("td");
-			 label.text(ra.stream_names[i+1]);
-			 label.classed(ra.stream_names[i+1], "true");
-			 for (var j = 0; j < stream_cooccurrence[i].length; j++) {
-				 myrow.append("td").text(stream_cooccurrence[i][j]);
-			 }
-		 }
-	}
+  // TODO: these functions create HTML elements from functions in rodalytics.js
+  // they should be replaced by a proper templating system!
 
-	function activitySummary(element,activitySummary) {
-		var mytable = element.append("table");
-		// could refactor & do forEach on streamname list?
-		var heading_row = mytable.append("tr");
-		heading_row.append("th").text("Stream").classed("All", true);
-		heading_row.append("th").text("Instances");
+  // Functions are passed an element to enrich & a dataset
 
-		activitySummary.forEach(function(e){
-			var myrow = mytable.append("tr");
-			var label = myrow.append("td");
-			label.text(e.stream_name);
-			label.classed(e.stream_name, "true");
+  ra.stats = (function () {
 
-			myrow.append("td").text(e.count);
-		})
-	}
+    // assumes the data is ordered firstly by time 
+    function cooccurrence(element, stream_cooccurrence) {
+      var mytable = element.append("table");
+       var heading_row = mytable.append("tr");
+       heading_row.append("th").text("Stream").classed("All", true);
+       for (var i = 0; i < 4; i++) {
+         var label = heading_row.append("th");
+         label.text(ra.stream_names[(i+1)]);
+         label.classed(ra.stream_names[(i+1)], "true");
+       };
+  
+       for (var i = 0; i < stream_cooccurrence.length; i++) {
+         var myrow = mytable.append("tr");
+         var label = myrow.append("td");
+         label.text(ra.stream_names[i+1]);
+         label.classed(ra.stream_names[i+1], "true");
+         for (var j = 0; j < stream_cooccurrence[i].length; j++) {
+           myrow.append("td").text(stream_cooccurrence[i][j]);
+         }
+       }
+    }
 
-	function durationPerStream(element, durationStats) {
-		 var mytable = element.append("table");
-		 var heading_row = mytable.append("tr");
-		 heading_row.append("th").text("Stream").classed("All", true);
-		 heading_row.append("th").text("Min");
-		 heading_row.append("th").text("Median");
-		 heading_row.append("th").text("Mean");
-		 heading_row.append("th").text("Std Dev");
-		 heading_row.append("th").text("Max");
+    function activitySummary(element,activitySummary) {
+      var mytable = element.append("table");
+      // could refactor & do forEach on streamname list?
+      var heading_row = mytable.append("tr");
+      heading_row.append("th").text("Stream").classed("All", true);
+      heading_row.append("th").text("Instances");
 
-		 var three_sf = d3.format(".3g");
+      activitySummary.forEach(function(e){
+        var myrow = mytable.append("tr");
+        var label = myrow.append("td");
+        label.text(e.stream_name);
+        label.classed(e.stream_name, "true");
+        myrow.append("td").text(e.count);
+      })
+    }
 
-		 durationStats.forEach(function(e){
-			 var myrow = mytable.append("tr");
-			 var label = myrow.append("td");
-			 label.text(e.stream_name);
-			 label.classed(e.stream_name, "true");
- 			 myrow.append("td").text(three_sf(e.min));
- 			 myrow.append("td").text(three_sf(e.median));
- 			 myrow.append("td").text(three_sf(e.mean));
- 			 myrow.append("td").text(three_sf(e.deviation));
- 			 myrow.append("td").text(three_sf(e.max));
-		 });
-	}
-	
-	function activityLog(element,data) {
-		 var mytable = element.append("table");
-		 var heading_row = mytable.append("tr");
-		 heading_row.append("th").text("Time");
-		 heading_row.append("th").text("Stream");
-		 heading_row.append("th").text("Comment");
+    function durationPerStream(element, durationStats) {
+       var mytable = element.append("table");
+       var heading_row = mytable.append("tr");
+       heading_row.append("th").text("Stream").classed("All", true);
+       heading_row.append("th").text("Min");
+       heading_row.append("th").text("Median");
+       heading_row.append("th").text("Mean");
+       heading_row.append("th").text("Std Dev");
+       heading_row.append("th").text("Max");
 
-		 data.forEach(function(d) {
-			 var myrow = mytable.append("tr");
-			 myrow.append("td").text(d.time).classed("time", "true");
-			 myrow.append("td").text(d.stream_name).classed(ra.stream_names[d.streamid], "true");
-			 myrow.append("td").text(d.comment).classed("comment", "true");
-		 })
-	}
-	
-	
-	return {
-		cooccurrence: cooccurrence,
-		activitySummary: activitySummary,
-		durationPerStream: durationPerStream,
-		activityLog: activityLog
-	}
-  };
-})(window, rodalytics);
+       var three_sf = d3.format(".3g");
+
+       durationStats.forEach(function(e){
+         var myrow = mytable.append("tr");
+         var label = myrow.append("td");
+         label.text(e.stream_name);
+         label.classed(e.stream_name, "true");
+         myrow.append("td").text(three_sf(e.min));
+         myrow.append("td").text(three_sf(e.median));
+         myrow.append("td").text(three_sf(e.mean));
+         myrow.append("td").text(three_sf(e.deviation));
+         myrow.append("td").text(three_sf(e.max));
+       });
+    }
+  
+    function activityLog(element,data) {
+       var mytable = element.append("table");
+       var heading_row = mytable.append("tr");
+       heading_row.append("th").text("Time");
+       heading_row.append("th").text("Stream");
+       heading_row.append("th").text("Comment");
+
+       data.forEach(function(d) {
+         var myrow = mytable.append("tr");
+         myrow.append("td").text(d.time).classed("time", "true");
+         myrow.append("td").text(d.stream_name).classed(ra.stream_names[d.streamid], "true");
+         myrow.append("td").text(d.comment).classed("comment", "true");
+       })
+    }
+
+    return {
+      cooccurrence: cooccurrence,
+      activitySummary: activitySummary,
+      durationPerStream: durationPerStream,
+      activityLog: activityLog
+    }
+  })();
+})(rodalytics);
