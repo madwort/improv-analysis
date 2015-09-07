1a730e4afd59ecdb73cd5323391d1e8ddd1fd381
Tom Ward <tomward@fmail.co.uk>
2015-09-07 08:40:54 +0100

    Bump version to 0.3

--- create_binary_package.sh
+++ create_binary_package.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-zip -r improv-analysis-vxxx.zip wp-plugin.php bower_components scripts style
+# zip -r improv-analysis-vxxx.zip wp-plugin.php bower_components scripts style
+zip -r improv-analysis-vxxx.zip wp-plugin.php scripts style
