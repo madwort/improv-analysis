e2f8980dc92c55d68515878f1c290def67a51d9e
Tom Ward <tomward@fmail.co.uk>
2015-09-14 13:32:37 +0100

    Bump version 0.13

--- wp-plugin.php
+++ wp-plugin.php
@@ -3,7 +3,7 @@
 Plugin Name: Improv Analysis
 Plugin URI: http://www.rodrigoconstanzo.com/thesis/
 Description: Package Improv Analysis tool for WP
-Version: 0.12
+Version: 0.13
 Author: MADWORT
 Author URI: http://www.madwort.co.uk
 */
